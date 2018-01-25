62fc1718abbbeab8629bf9f92d44d28ef2eef43b
mchome <280604399@qq.com>
2018-01-25 18:28:10 +0800

    [#3] basic zooming

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../../lib/flutter_advanced_networkimage.dart';
+import '../../lib/zoomable_widget.dart';
 
 void main() => runApp(new MyApp());
 
@@ -12,16 +14,25 @@ class MyApp extends StatelessWidget {
       ),
       home: new Scaffold(
         appBar: new AppBar(
-          title: const Text('Flutter Advanced Network Image'),
+          title: const Text('Flutter Advanced Network Image Example'),
         ),
-        body: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        body: new ZoomableWidget(
+          minScale: 0.8,
+          maxScale: 1.6,
+          child: new ListView(
             children: <Widget>[
-              new Container(),
+              new Image(
+                image: new AdvancedNetworkImage('https://user-images.githubusercontent.com/1551736/28209258-53234bf0-68c4-11e7-9586-d4a3526f0f45.png'),
+              ),
+              new Image(
+                image: new AdvancedNetworkImage('https://user-images.githubusercontent.com/1551736/28209258-53234bf0-68c4-11e7-9586-d4a3526f0f45.png'),
+              ),
+              new Image(
+                image: new AdvancedNetworkImage('https://user-images.githubusercontent.com/1551736/28209258-53234bf0-68c4-11e7-9586-d4a3526f0f45.png'),
+              ),
             ],
-          ),
-        ),
+          )
+        )
       ),
     );
   }
--- lib/zoomable_widget.dart
+++ lib/zoomable_widget.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
-class ZoomableWidget extends StatelessWidget {
-  ZoomableWidget({Key key}) : super(key: key);
+class ZoomableWidget extends StatefulWidget {
+  const ZoomableWidget({
+    Key key,
+    this.minScale: 0.7,
+    this.maxScale: 1.4,
+    this.enableZoom: true,
+    this.enablePan: true,
+    this.child,
+  }) : assert(minScale != null),
+        assert(maxScale != null),
+        assert(enableZoom != null),
+        assert(enablePan != null);
+
+  final double maxScale;
+  final double minScale;
+  final bool enableZoom;
+  final bool enablePan;
+  final Widget child;
+
+  @override
+  ZoomableWidgetState createState() => new ZoomableWidgetState();
+}
+
+class ZoomableWidgetState extends State<ZoomableWidget> {
+  double zoom = 1.0;
+  double previewZoom = 1.0;
+  Offset offset = Offset.zero;
+
+  _onScaleStart(ScaleStartDetails details) {
+    setState(() => offset = details.focalPoint);
+  }
+  _onScaleUpdate(ScaleUpdateDetails details) {
+    if (details.scale != 1.0) {
+      setState(() => zoom = (previewZoom * details.scale).clamp(widget.minScale, widget.maxScale));
+    }
+  }
+  _onScaleEnd(ScaleEndDetails details) {
+    previewZoom = zoom;
+  }
+  _handleReset() {
+    setState(() {
+      zoom = 1.0;
+      previewZoom = 1.0;
+      offset = Offset.zero;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    if (widget.child == null) return new Container();
+    return new GestureDetector(
+      child: _child(widget.child),
+      onScaleStart: widget.enableZoom ? _onScaleStart : null,
+      onScaleUpdate: widget.enableZoom ? _onScaleUpdate : null,
+      onScaleEnd: widget.enableZoom ? _onScaleEnd : null,
+    );
+  }
+
+  Widget _child(Widget child) {
+//    print('previewZoom: ' + previewZoom.toString());
+//    print('zoom: ' + zoom.toString());
+    return new FractionallySizedBox(
+      widthFactor: zoom,
+      child: child,
+    );
   }
 }
