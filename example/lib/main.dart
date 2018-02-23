9043184f0e98d7d12f3615c76e1a9286d3ed211d
mchome <280604399@qq.com>
2018-02-23 13:42:44 +0800

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,19 +13,16 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new DefaultTabController(
-        length: 2,
-        child: new Scaffold(
-          appBar: new AppBar(
-            title: const Text('Flutter Advanced Network Image Example'),
-          ),
-          body: new ZoomableWidget(
-            minScale: 0.3,
-            maxScale: 2.0,
-            child: new TransitionToImage(
-              new AdvancedNetworkImage(
-                  'https://user-images.githubusercontent.com/1551736/28209258-53234bf0-68c4-11e7-9586-d4a3526f0f45.png'),
-            ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Flutter Advanced Network Image Example'),
+        ),
+        body: new ZoomableWidget(
+          minScale: 0.3,
+          maxScale: 2.0,
+          child: new TransitionToImage(
+            new AdvancedNetworkImage(
+                'https://user-images.githubusercontent.com/1551736/28209258-53234bf0-68c4-11e7-9586-d4a3526f0f45.png'),
           ),
         ),
       ),
