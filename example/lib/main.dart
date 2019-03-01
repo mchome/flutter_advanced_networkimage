9c503dd36b387cc2ee955adc4b780cbd7be9d136
mchome <280604399@qq.com>
2019-03-01 14:57:40 +0800

    update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -8,8 +8,6 @@ import 'package:flutter_advanced_networkimage/provider.dart';
 import 'package:flutter_advanced_networkimage/cropper.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-// import 'package:flutter_advanced_networkimage/src/stream_loading_image.dart';
-
 void main() {
   runApp(MaterialApp(
     title: 'Flutter Example',
@@ -28,10 +26,6 @@ class Example extends State<MyApp> {
   final String svgUrl =
       'https://flutter.dev/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg';
 
-  Uint8List imageCropperData;
-
-  void cropImage(Uint8List data) => setState(() => imageCropperData = data);
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -46,136 +40,180 @@ class Example extends State<MyApp> {
               const Tab(text: 'zoomable widget'),
               const Tab(text: 'zoomable list'),
               const Tab(text: 'crop image(WIP)'),
-              // Tab(text: 'stream loading(DEMO)'),
             ],
           ),
         ),
         body: TabBarView(
           physics: NeverScrollableScrollPhysics(),
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                TransitionToImage(
-                  image: AdvancedNetworkImage(
-                    url,
-                    loadedCallback: () => print('It works!'),
-                    loadFailedCallback: () => print('Oh, no!'),
-                    // loadingProgress: (double progress) => print(progress),
-                    // disableMemoryCache: true,
-                  ),
-                  // loadedCallback: () => print('It works!'),
-                  // loadFailedCallback: () => print('Oh, no!'),
-                  // disableMemoryCache: true,
-                  fit: BoxFit.contain,
-                  placeholder: Container(
-                    width: 300.0,
-                    height: 300.0,
-                    color: Colors.transparent,
-                    child: const Icon(Icons.refresh),
-                  ),
-                  width: 300.0,
-                  height: 300.0,
-                  enableRefresh: true,
-                  loadingWidgetBuilder: (progress) {
-                    return Container(
-                      width: 300.0,
-                      height: 300.0,
-                      alignment: Alignment.center,
-                      child: Text(progress.toString()),
-                    );
-                  },
-                ),
-                Expanded(
-                  child: SvgPicture(
-                    AdvancedNetworkSvg(
-                      svgUrl,
-                      SvgPicture.svgByteDecoder,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            ZoomableWidget(
-              panLimit: 0.7,
-              maxScale: 2.0,
-              minScale: 0.5,
-              multiFingersPan: false,
-              // enableRotate: true,
-              autoCenter: true,
-              child: Image(image: AdvancedNetworkImage(url)),
-              // onZoomChanged: (double value) => print(value),
-            ),
-
-            Builder(builder: (BuildContext context) {
-              GlobalKey _key = GlobalKey();
-              return ZoomableList(
-                childKey: _key,
-                maxScale: 2.0,
-                child: Column(
-                  key: _key,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Image(image: AdvancedNetworkImage(url)),
-                    Image(image: AdvancedNetworkImage(url)),
-                    Image(image: AdvancedNetworkImage(url)),
-                  ],
-                ),
-              );
-            }),
-
-            Column(
-              children: <Widget>[
-                Container(
-                  height: 400.0,
-                  color: Colors.grey,
-                  child: ImageCropper(
-                    image: AdvancedNetworkImage(url),
-                    onCropperChanged: cropImage,
-                  ),
-                ),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      IconButton(
-                        color: Theme.of(context).primaryColor,
-                        icon: Icon(Icons.flip),
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        color: Theme.of(context).primaryColor,
-                        icon: Icon(Icons.crop),
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Cropped!'),
-                                content: SingleChildScrollView(
-                                  child: Container(
-                                    child: imageCropperData != null
-                                        ? Image.memory(imageCropperData)
-                                        : Container(),
-                                  ),
-                                ),
-                              );
-                            },
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-
-            // StreamLoadingImage(
-            //   url: url,
-            // ),
+            LoadImage(url: url, svgUrl: svgUrl),
+            ZoomableImage(url: url),
+            ZoomableImages(url: url),
+            CropImage(url: url),
           ],
         ),
       ),
+    );
+  }
+}
+
+class LoadImage extends StatelessWidget {
+  const LoadImage({@required this.url, @required this.svgUrl});
+
+  final String url;
+  final String svgUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransitionToImage(
+          image: AdvancedNetworkImage(
+            url,
+            loadedCallback: () => print('It works!'),
+            loadFailedCallback: () => print('Oh, no!'),
+            // loadingProgress: (double progress) => print(progress),
+            // disableMemoryCache: true,
+          ),
+          // loadedCallback: () => print('It works!'),
+          // loadFailedCallback: () => print('Oh, no!'),
+          // disableMemoryCache: true,
+          fit: BoxFit.contain,
+          placeholder: Container(
+            width: 300.0,
+            height: 300.0,
+            color: Colors.transparent,
+            child: const Icon(Icons.refresh),
+          ),
+          width: 300.0,
+          height: 300.0,
+          enableRefresh: true,
+          loadingWidgetBuilder: (progress) {
+            return Container(
+              width: 300.0,
+              height: 300.0,
+              alignment: Alignment.center,
+              child: Text(progress.toString()),
+            );
+          },
+        ),
+        Expanded(
+          child: SvgPicture(
+            AdvancedNetworkSvg(
+              svgUrl,
+              SvgPicture.svgByteDecoder,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ZoomableImage extends StatelessWidget {
+  const ZoomableImage({@required this.url});
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return ZoomableWidget(
+      panLimit: 0.7,
+      maxScale: 2.0,
+      minScale: 0.5,
+      multiFingersPan: false,
+      // enableRotate: true,
+      autoCenter: true,
+      child: Image(image: AdvancedNetworkImage(url)),
+      // onZoomChanged: (double value) => print(value),
+    );
+  }
+}
+
+class ZoomableImages extends StatelessWidget {
+  const ZoomableImages({@required this.url});
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    GlobalKey _key = GlobalKey();
+    return ZoomableList(
+      childKey: _key,
+      maxScale: 2.0,
+      child: Column(
+        key: _key,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image(image: AdvancedNetworkImage(url)),
+          Image(image: AdvancedNetworkImage(url)),
+          Image(image: AdvancedNetworkImage(url)),
+        ],
+      ),
+    );
+  }
+}
+
+class CropImage extends StatefulWidget {
+  const CropImage({@required this.url});
+
+  final String url;
+
+  @override
+  State<StatefulWidget> createState() => _CropImageState();
+}
+
+class _CropImageState extends State<CropImage> {
+  Uint8List imageCropperData;
+
+  void cropImage(Uint8List data) => setState(() => imageCropperData = data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 400.0,
+          color: Colors.grey,
+          child: ImageCropper(
+            image: AdvancedNetworkImage(widget.url),
+            onCropperChanged: cropImage,
+          ),
+        ),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(
+                color: Theme.of(context).primaryColor,
+                icon: Icon(Icons.flip),
+                onPressed: () {},
+              ),
+              IconButton(
+                color: Theme.of(context).primaryColor,
+                icon: Icon(Icons.crop),
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('Cropped!'),
+                        content: SingleChildScrollView(
+                          child: Container(
+                            child: imageCropperData != null
+                                ? Image.memory(imageCropperData)
+                                : Container(),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
