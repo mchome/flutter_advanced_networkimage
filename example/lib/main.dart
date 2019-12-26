e12f83026a08efc28f93636105580cb58f86f60b
Ewerton Ramos <contato@ewertonrp.com>
2019-12-26 20:30:23 -0300

    Fix tab length

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,7 +29,7 @@ class Example extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4,
+      length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Advanced Network Image Example'),
