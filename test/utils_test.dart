7005a87b070b0475c99228d0edb9ac38fd4137ed
mchome <280604399@qq.com>
2020-01-21 10:49:17 +0800

    replace test url

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -23,7 +23,7 @@ void main() {
           result);
 
       url =
-          'https://flutter.dev/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg';
+          'https://github.com/dart-lang/site-shared/raw/master/src/_assets/image/flutter/logo/default.svg';
       result = (await http.get(url)).bodyBytes;
 
       expect(
@@ -53,7 +53,7 @@ void main() {
 
       url = 'this is another label';
       realUrl =
-          'https://flutter.dev/assets/flutter-lockup-4cb0ee072ab312e59784d9fbf4fb7ad42688a7fdaea1270ccf6bbf4f34b7e03f.svg';
+          'https://github.com/dart-lang/site-shared/raw/master/src/_assets/image/flutter/logo/default.svg';
       result = (await http.get(realUrl)).bodyBytes;
 
       expect(
